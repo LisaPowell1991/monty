4362b6568d73c4afc0463cd6ea2ff276292c7ac0
LisaPowell1991 <lisapowell46@gmail.com>
2023-08-24 03:12:18 -0700

    swap opcode

--- opcodes_functions2.c
+++ opcodes_functions2.c
@@ -10,14 +10,17 @@
 
 void swap(stack_t **stack, unsigned int line_num)
 {
-	int tmp;
+	stack_t *top_node = NULL;
+	int top_value = 0;
 
-	if (!stack || !(*stack) || !((*stack)->next))
+	if (!stack || !*stack || !((*stack)->next))
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
 	}
-	tmp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = tmp;
+	top_node = *stack;
+	top_value = top_node->n;
+
+	top_node->n = top_node->next->n;
+	top_node->next->n = top_value;
 }
